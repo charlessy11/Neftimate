c142c13af970de81ec8a633010b164aef326def2
Adrian Smith <acsmith11@dons.usfca.edu>
2022-03-09 17:31:36 -0800

    initial db-setup

---
+++ store/migrations/20220310013034_create-db.js
@@ -0,0 +1,17 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  knex.schema.createTable("collections", tbl => {
+      tbl.increments();
+  })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+};
