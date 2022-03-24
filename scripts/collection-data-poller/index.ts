6ba5a657efd208c609b0d76fc5aa6981c099a6fd
selviipalani <selviipalani27@gmail.com>
2022-03-23 22:04:27 -0700

    started data puller, need to parse data still

--- scripts/collection-data-poller/index.ts
+++ scripts/collection-data-poller/index.ts
@@ -1,1 +1,54 @@
-console.log("Hello")
+import axios from 'axios'
+import { json } from 'node:stream/consumers';
+import * as getAssetsRes from './responses/getAssets.json'
+
+
+const { COLLECTION_SLUG } = process.env;
+
+const getCollectionData = async () => {
+    let res;
+    try {
+        res = await axios.get(`https://api.opensea.io/api/v1/assets?collection_slug=${COLLECTION_SLUG}`)
+    } catch (error) {
+        res = 'nokey'
+    }
+
+    // Call function to parse data here
+    const parsed = parseResponse(res) 
+
+    return parsed
+
+}
+
+const parseResponse = (response: any) => {
+    if (response == 'nokey'){
+        response = getAssetsRes
+    }
+
+    if (response == null) return {}
+
+    parseAssets(response.assets)
+
+
+    return response
+
+}
+
+const parseAssets = (assets : Array<any>) =>{
+    //loop thru assets
+    assets.map(asset => {
+        // console.table(asset)
+        console.log(asset.id);
+        console.log(asset.collection_id) //no collection_id in response
+        console.log(asset.traits)
+        console.log(asset.listing_date)
+        console.log(asset.name)
+        console.log(asset.img_url)
+    })
+}
+
+
+(async () => {
+    const data = await getCollectionData();
+    // console.log(`TEST: ${JSON.stringify(data)}`);
+})()
---
+++ scripts/collection-data-poller/types.ts
@@ -0,0 +1,22 @@
+interface Collection {
+    id: string
+    name: string
+    market_cap: number
+    total_supply: number
+    updated_at: Date
+    traits: Array<Object>
+    slug: string
+    contract_address: string
+    img_url: string
+}
+
+
+interface Asset {
+    id: string
+    collection_id: string
+    traits: Array<Object>
+    listing_date: Date
+    name: string 
+    rarity_score: number
+    img_url: string
+}
