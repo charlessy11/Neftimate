97034ba066b7df2b1cf6e21c4701d89f085414b4
Adrian Smith <acsmith11@dons.usfca.edu>
2022-05-10 23:35:31 -0700

    documentation

--- api/index.ts
+++ api/index.ts
@@ -9,13 +9,9 @@ const port = 3000
 
 
 // GET Prediction
-app.get('/prediction', (req, res) => {
-    if (!req.query.asset_id){
-        res.status(httpStatus.BAD_REQUEST).send({error: "Please include the asset_id of the asset you would like to get a prediction for"})
-        return;
-    }
+app.get('/prediction/:id', (req, res) => {
 
-    const asset_id = req.query.asset_id
+    const asset_id = req.params.id
 
     db('predictions').where({ asset_id: asset_id }).then((rows: any) => {
         if (rows.length == 0){
@@ -30,7 +26,7 @@ app.get('/prediction', (req, res) => {
     })
     .catch((error: any) => {
         console.error(error)
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send({error: `Error fetching predictions for asset_id ${asset_id}`})
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send({error: `Internal Server Error`})
     })
 })
 
@@ -40,8 +36,46 @@ app.get('/collections', (req, res) => {
         res.status(httpStatus.OK).send(rows)
     })
     .catch((error: any) => {
-        res.status(httpStatus.INTERNAL_SERVER_ERROR).send({error: `Error fetching collection data: ${error}`})
+        console.error(error)
+        res.status(httpStatus.INTERNAL_SERVER_ERROR).send({error: "Internal Server Error"})
     })
+})
+
+// GET Collection assets by slug QP: limit
+app.get('/collections/:slug', (req, res) => {
+    const slug = req.params.slug
+    let limit = 50
+
+    if (req.query.limit && typeof(req.query.limit) == 'string'){
+        limit = isNaN(parseInt(req.query.limit)) ? 50 : parseInt(req.query.limit)
+    }
+
+    if (limit > 50 || limit < 0){
+        res.status(405).send({error: "Limit parameter must be greater than 0 and less than 50"})
+        return;
+    }
+
+    const columns = ['id', 'traits', 'name', 'rarity_score', 'img_url', 'collection_address']
+
+    db('collections').select('address').where({slug: slug})
+        .then((rows: any) => {
+            if (rows < 1){
+                res.status(410).send({error: "Collection not found"})
+                return;
+            }
+            return rows[0].address
+        })
+        .then((address: string ) => {
+            db('assets').select(columns).where({collection_address: address}).limit(limit)
+            .then((rows: any) => {
+                console.log(address)
+                res.status(httpStatus.OK).send(rows)
+            })
+        })
+        .catch((error: any) => {
+            console.error(error)
+            res.status(httpStatus.INTERNAL_SERVER_ERROR).send({error: "Internal Server Error"})
+        })
 })
 
 
@@ -56,11 +90,9 @@ app.get('/assets', (req, res) => {
 
     if (limit > 50 || limit < 0){
         res.status(httpStatus.BAD_REQUEST).send({error: "Limit parameter must be greater than 0 and less than 50"})
-        return;
     }
 
     db('assets').select(['id', 'traits', 'name', 'img_url', 'rarity_score', 'collection_address']).limit(limit).then((rows: any) => {
-        console.log(`Retreived ${rows.length}`)
         res.status(httpStatus.OK).send(rows);
     })
     .catch((error: any) => {
@@ -73,7 +105,7 @@ app.get('/assets', (req, res) => {
 // GET /assets/id
 app.get("/assets/:id", (req, res) => {
     const assetId = req.params.id
-    const columns = ['id', 'traits', 'name', 'img_url', 'collection_address']
+    const columns = ['id', 'traits', 'name', 'rarity_score', 'img_url', 'collection_address']
 
     db('assets').select(columns).where({id: assetId}).then((rows: any) => {
         res.status(httpStatus.OK).send(rows)
